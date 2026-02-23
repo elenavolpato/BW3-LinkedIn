92bf15610946108a4593a857fe18b25b51810df3
Andrei Briceag <andrei98briceag@gmail.com>
2026-02-23 17:15:06 +0100

    Static nabhovercss

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -76,7 +76,7 @@ const TopBar = () => {
                     <p>fasdfdasfasdfsdsfaadsfasfsdaffafdsfdsaf</p>
                   </div>
                 </div>
-                <Button className=" d-block w-100 text-primary bg-light rounded-5 py-0 buttonViewProfile">Visualizza profilo</Button>
+                <Button className=" d-block w-100 text-primary bg-light rounded-5 py-0 buttonViewProfile fw-bold">Visualizza profilo</Button>
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#settings">
