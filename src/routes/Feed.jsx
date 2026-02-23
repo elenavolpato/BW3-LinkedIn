adf60fd5e2f5cbf60430391e380892683ed19f5f
Dedes <federico.lepore73@gmail.com>
2026-02-23 18:16:06 +0100

    loading and error

--- src/routes/Feed.jsx
+++ src/routes/Feed.jsx
@@ -1,12 +1,16 @@
 import PostCard from "../components/PostCard"
 import { useEffect, useState } from "react"
-import "./feed.css"
 import { Container, Col, Row } from "react-bootstrap"
+import Alert from "react-bootstrap/Alert"
+import { Spinner } from "react-bootstrap"
+import "./feed.css"
 
 
 const Feed = () => {
 
   const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   const fetchURL = 'https://striveschool-api.herokuapp.com/api/posts/'
   const apiKEy = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTljMTE1ODBiYzFkZTAwMTU3N2I3OWQiLCJpYXQiOjE3NzE4MzU3MzYsImV4cCI6MTc3MzA0NTMzNn0.sqNA4zaClXn_qt6yLcLVvYsT1sOeGx_2BmLmdLBSF40'
@@ -27,9 +31,12 @@ const Feed = () => {
       .then((data) => {
         console.log(data)
         setPosts(data);
+        setLoading(false)
       })
       .catch((err) => {
         console.log(err)
+        setError(err.message)
+        setLoading(false)
       })
   }
 
@@ -45,29 +52,33 @@ const Feed = () => {
 
         {/* colonna sx */}
         <Col md={3}>
-        </Col>
-
-        <Col md={6}>
-          {posts.map((post) => (
-            <PostCard
-              key={post._id}
-              id={post._id}
-              name={
-                post.user?.name && post.user.name.trim() !== ""
-                  ? post.user.name
-                  : "Ero troppo pigro per metterlo"
-              }
-              position={post.user.title}
-              date={new Date(post.createdAt).toLocaleDateString()}
-              description={post.text}
-              image={post.image}
-              video={post.video}
-            />
-          ))}
 
         </Col>
+
+        {loading ? (<Spinner animation="border" />) : error ? (<Alert variant="danger" className="text-center">{error}</Alert>) : (
+          <Col md={6}>
+            {posts.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                name={
+                  post.user?.name && post.user.name.trim() !== ""
+                    ? post.user.name
+                    : "Ero troppo pigro per metterlo"
+                }
+                position={post.user.title}
+                date={new Date(post.createdAt).toLocaleDateString()}
+                description={post.text}
+                image={post.image}
+                video={post.video}
+              />
+            ))}
+          </Col>)}
+
         {/* colonna dx */}
-        <Col md={3}></Col>
+        <Col md={3}>
+
+        </Col>
       </Row>
     </Container>
   )
