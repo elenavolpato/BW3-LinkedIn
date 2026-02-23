cc855bfc66c50b118eb64fee38bd6745ec859c54
Ernesto <ernesto.scorzelli77@gmail.com>
2026-02-23 15:11:34 +0100

    primo merge

--- src/routes/Feed.jsx
+++ src/routes/Feed.jsx
@@ -4,17 +4,19 @@ import RightSideBar from "../components/RightSideBar";
 
 const Feed = () => {
   return (
-    <div className="feed">
-      <h1>Feed</h1>
-      <PostCard
-        id={1}
-        name="Mario Rossi"
-        position="Software Engineer"
-        date="1 Gennaio 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
+    <>
+      <div className="feed d-flex">
+        <h1>Feed</h1>
+        <PostCard
+          id={1}
+          name="Mario Rossi"
+          position="Software Engineer"
+          date="1 Gennaio 2024"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+      </div>
       <RightSideBar />
-    </div>
+    </>
   );
 };
 export default Feed;
