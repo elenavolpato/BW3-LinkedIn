6c9e07a2ea03df28f42c75b87b9ae1a5deb5dcec
Andrei Briceag <andrei98briceag@gmail.com>
2026-02-23 16:38:01 +0100

    icons in html

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./routes/Feed";
 import Profile from "./routes/Profile";
