7b1bb093a60668aa4b5c4f89ed2c234da2e1f05b
Andrei Briceag <andrei98briceag@gmail.com>
2026-02-23 12:27:46 +0100

    Navchanges

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feed from "./routes/Feed";
+import Profile from "./routes/Profile";
 import TopBar from "./components/TopBar";
 
 function App() {
@@ -10,8 +12,8 @@ function App() {
       <BrowserRouter>
         <TopBar />
         <Routes>
-          {/* <Route path="/" element={} />
-          <Route path="/profile" element={} /> */}
+          <Route path="/" element={<Feed />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,39 +1,43 @@
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-import "./TopBar.css";
+import "../assets/css/TopBar.css";
 
 const TopBar = function () {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className=" bg-body-light">
         <Container>
-          {" "}
-          {/* NOT fluid */}
           <Navbar.Brand href="#">
             <i className="bi bi-linkedin text-primary fs-2"></i>
           </Navbar.Brand>
-          <div className="linkedin-search">
-            <i className="bi bi-search search-icon"></i>
-            <Form.Control type="search" placeholder="Search" className="search-input" />
+          <div className="linkedin-search-container">
+            <div className="linkedin-search">
+              <i className="bi bi-search search-icon"></i>
+              <Form.Control type="search" placeholder="Cerca" className="search-input" />
+            </div>
           </div>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <Nav.Link href="#" disabled>
-                Link
+              <Nav.Link href="#action1" className="d-flex flex-column align-items-center nav-item-custom">
+                <i className="bi bi-house-door-fill nav-icon"></i>
+                <span className="nav-text">Home</span>
               </Nav.Link>
+
+              <Nav.Link href="#action2" className="d-flex flex-column align-items-center nav-item-custom">
+                <i className="bi bi-people-fill nav-icon"></i> {/* esempio altra icona */}
+                <span className="nav-text">La mia rete</span>
+              </Nav.Link>
+
+              {/* altri item allo stesso modo */}
             </Nav>
 
             <Form className="d-flex">
-              <Button variant="outline-success">Search</Button>
-              <NavDropdown title="Tu" id="navbarScrollingDropdown">
+              <NavDropdown title="Tu" id="navbarScrollingDropdown" className="fs-6">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
                 <NavDropdown.Divider />
