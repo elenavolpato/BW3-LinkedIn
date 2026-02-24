402b2f79214e2893ed623fe03645c50a4a247be5
raposinha <elena@raposinha.dev>
2026-02-24 16:01:12 +0100

    fixes conflicts

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import { Container, Row, Col } from "react-bootstrap"
-=======
-import { Container } from "react-bootstrap"
->>>>>>> elena-experience
 import ExperienceList from "../components/ExperienceList"
 import PersonalInfo from "../components/PersonalInfo"
 import RightSidebarProfile from "../components/RightSidebarProfile"
 
 const Profile = () => {
   return (
-<<<<<<< HEAD
     <Container className="pt-3">
       <Row>
         <Col
@@ -28,11 +23,6 @@ const Profile = () => {
           <RightSidebarProfile />
         </Col>
       </Row>
-=======
-    <Container>
-      <PersonalInfo />
-      <ExperienceList />
->>>>>>> elena-experience
     </Container>
   )
 }
