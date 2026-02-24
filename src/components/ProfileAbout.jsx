8d37701ee75768df8795a1ff156854cf0e5a243d
Dedes <federico.lepore73@gmail.com>
2026-02-24 11:22:35 +0100

    ProfileAbout

--- src/components/ProfileAbout.jsx
+++ src/components/ProfileAbout.jsx
@@ -1,7 +1,22 @@
+import '../assets/css/ProfileAbout.css'
+
 function ProfileAbout() {
     return (
-        <div>
-            <h4>Abaut, of course, me, because is my profail</h4>
+        <div className="about-container">
+            <h4 className="mb-0">Abaut</h4>
+            <p className="m-0">Wot?? Of course: ME! because is my profail</p>
+
+            <p className="mt-2">Ciao! Sono io! Quello del profilo. Ho esperienza e competenze molto varie, so:
+                <ul>
+                    <li>leggere</li>
+                    <li>scrivere</li>
+                    <li>parlare</li>
+                    <li>ascoltare</li>
+                    <li>camminare</li>
+                    <li>a volte anche correre</li>
+                </ul>
+                Anche se ogni tanto, lo ammetto, mi dimentico di respirare, ma è solo perché sono così concentrato su quello che sto facendo.
+                <br /> Es iu can si Ai'm veri gud in inglisc. Ai studiei in a scull very very inglish. </p>
         </div>
     )
 }
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,12 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap"
 import PersonalInfo from "../components/PersonalInfo"
 import ProfileAbout from "../components/ProfileAbout"
 const Profile = () => {
   return (
-    <div>
+    <Container className="profile-container">
 
-      <PersonalInfo />
-      <ProfileAbout />
-    </div>
+      <Row className="justify-content-center">
+
+        <Col md={12} lg={9}>
+
+          <PersonalInfo />
+          <ProfileAbout />
+
+        </Col>
+
+        <Col lg={3}></Col>
+
+      </Row>
+
+    </Container>
   )
 }
 export default Profile
